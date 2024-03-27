96b88d68c9124ed5d3fc662c664caace5f0c4ed6
oscar <oscar_resendiz@hotmail.com>
2024-03-26 20:34:24 -0600

    se sube codigo gpio

--- ControlMaquinaMolde/scr/GPIO/gpio.py
+++ ControlMaquinaMolde/scr/GPIO/gpio.py
@@ -1,4 +1,4 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 #la libreria para el control de los pines es "pip3 install rpi.gpio"
 #si no funciona ese comando hay que intentarlo on este
 #sudo apt install python3-rpi.gpio
@@ -7,16 +7,22 @@
 class GpioClass():
     #-------------------------------------------------------Constructor-------------------------------------------------------------
     def __init__(self):
-        #se inicializa los pines que se nececiten
-        self.Pin12=12
-        GPIO.cleanup()
-        #confoguro los pines
-        GPIO.setmode(GPIO.BOARD)
-        #se configura el pin 12 como entrada
-        GPIO.setup(self.Pin12,GPIO.IN)
+        try:
+            #se inicializa los pines que se nececiten
+            self.Pin12=12
+            GPIO.cleanup()
+            #confoguro los pines
+            GPIO.setmode(GPIO.BOARD)
+            #se configura el pin 12 como entrada
+            GPIO.setup(self.Pin12,GPIO.IN)
+        except Exception as err:        
+            print(f"ocurrio un error en GpioClass:__init__ {err=}, {type(err)=}")    
     #-------------------------------------------------------maquinaStatus-------------------------------------------------------------
     def maquinaStatus(self):
-        #regresa el estatus de la maquina
-        valor=GPIO.input(self.Pin12)
-        #valor=1
-        return valor
+        try:
+            #regresa el estatus de la maquina
+            valor=GPIO.input(self.Pin12)
+            #valor=1
+            return valor
+        except Exception as err:        
+            print(f"ocurrio un error en GpioClass:maquinaStatus {err=}, {type(err)=}")    
